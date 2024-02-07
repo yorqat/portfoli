16b3969d9f89220fba6e53f58ef4acd73abc11b1
YorQat <qarkdev+gh@gmail.com>
2024-02-07 14:36:04 +0800

    embed fix

--- src/lib/MetaHead.svelte
+++ src/lib/MetaHead.svelte
@@ -24,5 +24,6 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:image" content={image} />
+	<meta property="og:type" content="website" />
 	<!-- {@render children()} -->
 </svelte:head>
