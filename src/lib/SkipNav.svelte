3f109c8300b71865160410d64afde8c638cc6ed6
yorqat <qarkdev+gh@gmail.com>
2024-02-11 21:24:50 +0800

    better skip to content

--- src/lib/SkipNav.svelte
+++ src/lib/SkipNav.svelte
@@ -1,27 +1,30 @@
 <a href="#content">Skip to main content</a>
 
 <style>
-    a {
-        all: unset;
-        display: block;
-        position: absolute;
-        height: 2rem;
-        padding: 1rem;
-        margin: 1rem;
-        border-radius: 12px;
-        background-color: var(--bg);
-        color: var(--clr);
-        font-family: sans-serif;
-        font-weight: 800;
-        text-transform: uppercase;
-        transform: translateY(-200%);
-        transition: transform 200ms;
+	a {
+		display: block;
+		position: absolute;
+		padding: 0.5rem;
+		margin: 0.25rem;
+		background-color: var(--bg);
+		color: white;
+		font-family: sans-serif;
+		font-weight: 800;
+		text-transform: uppercase;
+		transform: translateY(-200%);
+		transition: transform 200ms;
+		text-align: center;
 
-        z-index: 2;
-        grid-area: 1 / 1 / 2 / 4;
-    }
+		z-index: 2;
+		grid-area: 1 / 1 / 2 / 4;
 
-    a:focus {
-        transform: translateY(0);
-    }
+		background-color: var(--bg2);
+		padding: 1.2rem 1rem;
+		width: 20rem;
+		background-color: var(--header-bg);
+	}
+
+	a:focus {
+		transform: translateY(0);
+	}
 </style>
