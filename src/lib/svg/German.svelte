c7c06796423ffd2d41e0a2d47b7bcff9c96fd1f6
yorqat <qarkdev+gh@gmail.com>
2024-02-25 07:39:27 +0800

    en-de lang initial support

---
+++ src/lib/svg/German.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	const { label = '' } = $props();
+
+	let lang: 'EN' | 'DE' = 'EN';
+
+	const onclick = () => {};
+	const keydown = () => {};
+</script>
+
+<label id={label} for="{label}-input">{label}</label>
+
+<div
+	class="german-eng"
+	id="{label}-input"
+	{onclick}
+	on:keydown={keydown}
+	role="switch"
+	aria-labelledby={label}
+	aria-checked={lang}
+	tabindex="0"
+>
+	<span class="lang en">EN</span>
+	<span class="separator"> | </span>
+	<span class="lang de">DE</span>
+</div>
+
+<style>
+	.separator {
+		font-size: 2rem;
+	}
+
+	.en {
+		color: var(--clr2);
+	}
+
+	.de {
+		color: var(--clr);
+	}
+
+	.german-eng {
+		width: 3.25rem;
+		display: flex;
+		align-items: center;
+	}
+</style>
