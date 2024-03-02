50035d62c8320944e48909509d4d9dac2c7bfbe4
yorqat <qarkdev+gh@gmail.com>
2024-03-02 19:41:19 +0800

    revised guideline

---
+++ src/lib/svg/GuideLine.svelte
@@ -0,0 +1,8 @@
+<svg width="20000" height="2" viewBox="0 0 20000 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M20000 1L-0.000974047 1"
+		stroke="#E4DADA"
+		stroke-opacity="0.21"
+		stroke-dasharray="33 33"
+	/>
+</svg>
